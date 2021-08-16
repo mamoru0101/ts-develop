6426432f712ba8812cdda60d1e257db960f4c418
mamoru0101 <kmamoru0101@gmail.com>
2021-08-16 19:05:57 +0900

    object

---
+++ src/object.ts
@@ -0,0 +1,13 @@
+export {}
+
+
+
+let profile1: object = {name: "kdm"} 
+profile1 = {birthYear: 1993}
+
+
+let profile2: {
+  name: string
+} = {name: "kdm"}
+// profile2 = {birthYear: 1993}
+profile2 = {name: "mmr"}
