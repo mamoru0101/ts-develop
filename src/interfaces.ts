9961edf21b382860e17a6d08b38fcee34ca14645
mamoru0101 <kmamoru0101@gmail.com>
2021-08-19 14:06:12 +0900

    interface

---
+++ src/interfaces.ts
@@ -0,0 +1,18 @@
+export {}
+
+type ObjectType = {
+  name: string;
+  age: number;
+}
+
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+
+let object: ObjectType = {
+  name: 'kdm',
+  age: 32
+}
