7e0501771d7ae2711509143aa6e28d07d4537b02
mamoru0101 <kmamoru0101@gmail.com>
2021-08-19 15:13:03 +0900

    シグネチャ

---
+++ src/overloads.ts
@@ -0,0 +1,16 @@
+export {}
+
+// シグネチャ
+function double(value: number): number
+function double(value: string): string
+
+function double(value: any): any {
+  if(typeof value === 'number'){
+    return value * 2
+  }
+  return value + value
+}
+
+console.log(double(100))
+console.log(double("hello "))
+// console.log(double(true))
